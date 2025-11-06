c8a167d526a4e025cfe0ceadbafc47606a515447
sabinadjumayeva01-cyber <sabinadjumayeva01@gmail.com>
2025-11-06 15:15:44 +0500

    editing

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py - bot configuration
 BOT_TOKEN = "8115188986:AAGEpkUIAVGYtrKuYnH83zvIVAbZbPn8q2I"
-ADMIN_ID = 6106674802, 1184042910
+ADMIN_ID = 1184042910
 DB_PATH = "bot.db"
 MEDIA_DIR = "data/lessons"
